c267495aa7d8a9fb3b1c75871efd40e715ae66fd
hino <asonehino@naver.com>
2024-06-03 09:32:25 +0900

    basic_socket2_second.py

--- basic_dictionary.py
+++
@@ -1,9 +0,0 @@
-nation = {'Korea':"+82", 'US':"+1", 'Japan': "+81"}
-print(nation['Korea'])
-
-var = input()
-
-if (var in nation):
-    print('country code = ', nation['US'])
-else:
-    print('something wrong')
