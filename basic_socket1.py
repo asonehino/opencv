629f546f0f9e58f5ad1ec8b4428aa480d88237ff
hino <asonehino@naver.com>
2024-06-03 09:15:17 +0900

    basic_socket1.py

---
+++ basic_socket1.py
@@ -0,0 +1,14 @@
+#server code
+from socket import *
+
+host = '10.129.58.175'
+port = 9999
+
+client_socket = socket(AF_INET, SOCK_STREAM)
+client_socket.connect((host, port))
+client_socket.sendall('안녕'.encode())
+
+data = client_socket.recv(1024)
+print('received from', repr(data.decode()))
+
+client_socket.close()
